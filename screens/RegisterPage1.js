56c48124bb8b4c01afe9a0485f7a5375d5e406f8
Byttemos <hriskaer@gmail.com>
2022-11-17 12:57:32 +0100

    Implement camera access

--- screens/RegisterPage1.js
+++ screens/RegisterPage1.js
@@ -1,39 +1,43 @@
-import { View, Text, Image } from 'react-native'
+import { View, Text, Image, Button } from 'react-native'
 import React from 'react'
 
-export default function RegisterScreen({navigation}) {
+export default function RegisterScreen({ navigation }) {
     return (
-      <View style={{ flex: 1, alignItems: 'center', backgroundColor: '#FFBDF1'}}>
-        <View style={{flex:1}}>
-            <Text style={{fontSize: 20, margin: 5}}>
-                DEN ANDEN HANDSKE
-            </Text>
-        </View>
-        <View style={{flex:3}}>
-            <Text>
-                Take a picture of the glove
-            </Text>
-            <View style={{alignContent: 'center'}}>
-                <Image 
-                    style={{height: 100, width: 100, justifyContent: 'center', alignContent: 'center', alignItems: 'center', borderRadius: 10, margin: 10}}
-                    source={require('../assets/cameraicon.png')} 
-                />
+        <View style={{ flex: 1, alignItems: 'center', backgroundColor: '#FFBDF1' }}>
+            <View style={{ flex: 1 }}>
+                <Text style={{ fontSize: 20, margin: 5 }}>
+                    DEN ANDEN HANDSKE
+                </Text>
             </View>
-            
+            <View style={{ flex: 3 }}>
+                <Text>
+                    Take a picture of the glove
+                </Text>
+                <View style={{ alignContent: 'center' }}>
+                    <Image
+                        style={{ height: 100, width: 100, justifyContent: 'center', alignContent: 'center', alignItems: 'center', borderRadius: 10, margin: 10 }}
+                        source={require('../assets/cameraicon.png')}
+                    />
+                    <Button
+                        title='open camera'
+                        onPress={() => navigation.navigate('Camera')}
+                    />
+                </View>
 
-        </View>
-        <View style={{flex:1}}>
-            <Text>
-                hej
-            </Text>
 
-        </View>
-        <View style={{flex:1}}>
-            <Text>
-                hej
-            </Text>
+            </View>
+            <View style={{ flex: 1 }}>
+                <Text>
+                    hej
+                </Text>
 
+            </View>
+            <View style={{ flex: 1 }}>
+                <Text>
+                    hej
+                </Text>
+
+            </View>
         </View>
-      </View>
     );
-  }
+}
