08938c63a0b43887bd4e5c9846d2b7b2e89364eb
Marie Riskær <mriskaer@gmail.com>
2022-11-17 10:12:03 +0100

    Setup

--- App.js
+++ App.js
@@ -1,20 +1,61 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { View, Text, Image, Button, Pressable } from 'react-native';
+import { NavigationContainer, useLinkProps } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { useFonts } from 'expo-font';
 
-export default function App() {
+function HomeScreen({navigation}) {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: 'pink' }}>
+      <Text style={{ fontSize: 40, textAlign: 'center' /* , fontFamily: 'Inria Sans' */}}> 
+      DEN ANDEN HANDSKE 
+      </Text>
+      <Image source={require('./assets/frontpage.png')} />
+      <Button 
+        title="Register glove"
+        onPress={() => navigation.navigate('Register')}
+      />
+
+      <Button
+        /* style={{flex: 1, borderRadius: 20, flexDirection: 'row', backgroundColor: 'blue', width: 200}} */
+        title="Find glove"
+        onPress={() => navigation.navigate('Find')}
+      />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function RegisterScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Register glove screen</Text>
+    </View>
+  );
+}
+
+function FindScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Find glove screen</Text>
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  /* const [fontsLoaded] = useFonts({
+    'Inria Sans': require('./assets/fonts/InriaSans-Light.ttf'),
+  }); */
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} options={{ title: '' }}/>
+        <Stack.Screen name="Register" component={RegisterScreen}/>
+        <Stack.Screen name="Find" component={FindScreen}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
---
+++ screens/StartScreen.js
@@ -0,0 +1,9 @@
+import { View } from "react-native";
+
+import { View, Button, Image, Text} from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+/* export default function StartScreen(navigation){
+    const [getComments, setComments, getPosts, setPosts] = useContext(CommentContext);
+} */
